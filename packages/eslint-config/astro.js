a0a7acb7769c4b0803aaf2eec51c0a50bfc13259
Lucas Sá <lucas.sodresa@gmail.com>
2025-06-13 20:47:40 +0100

    chore: add eslint for astro

---
+++ packages/eslint-config/astro.js
@@ -0,0 +1,9 @@
+import { config as reactConfig } from "./react-internal.js";
+
+/**
+ * A custom ESLint configuration for libraries that use React.
+ *
+ * @type {import("eslint").Linter.Config[]} */
+export const config = [
+    ...reactConfig
+];
