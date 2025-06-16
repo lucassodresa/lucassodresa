d99bcdb8185ecf5f91658117dd018bb156390408
Lucas Sá <lucas.sodresa@gmail.com>
2025-06-16 21:03:26 +0100

    chore(sanity-cms): format files

--- apps/sanity-cms/eslint.config.js
+++ apps/sanity-cms/eslint.config.js
@@ -1,5 +1,5 @@
-import { config as baseConfig } from "@lucassodresa/eslint-config/base";
-import studio from '@sanity/eslint-config-studio'
+import { config as baseConfig } from '@lucassodresa/eslint-config/base';
+import studio from '@sanity/eslint-config-studio';
 /** @type {import("eslint").Linter.Config} */
 
 export default [...baseConfig, ...studio];
