cd84fdd5e29026ed40eb964ffe0af318d3bcee17
Lucas Sá <lucas.sodresa@gmail.com>
2025-06-16 10:18:10 +0100

    chore(eslint-config): add format script and formatting

--- packages/eslint-config/astro.js
+++ packages/eslint-config/astro.js
@@ -1,9 +1,7 @@
-import { config as reactConfig } from "./react-internal.js";
+import { config as reactConfig } from './react-internal.js';
 
 /**
  * A custom ESLint configuration for libraries that use React.
  *
  * @type {import("eslint").Linter.Config[]} */
-export const config = [
-    ...reactConfig
-];
+export const config = [...reactConfig];
